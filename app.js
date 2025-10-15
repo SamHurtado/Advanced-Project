19a99f712e46f5f474ff1bb0c94cb59ade626925
SamHurtado <samhurtado@icloud.com>
2025-10-15 14:51:37 -0400

    Update

---
+++ app.js
@@ -0,0 +1,30 @@
+// template_2214fxg
+
+// service_adyukod
+
+// UPnKTCZcG4nJ5s_pU
+
+// <script type="text/javascript"
+//         src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
+// </script>
+// <script type="text/javascript">
+//    (function(){
+//       emailjs.init({
+//         publicKey: "UPnKTCZcG4nJ5s_pU",
+//       });
+//    })();
+// </script>
+
+function contact(event) {
+    event.preventDefault();
+    emailjs
+        .sendForm(
+            'service_adyukod',
+            'template_2214fxg',
+            event.target,
+            'user_UPnKTCZcG4nJ5s_pU'
+        ).then(() => {
+            console.log('this worked1')
+        })
+
+}
